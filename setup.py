abdcac4a3266a40fd1ebda28857d4e502a320051
DPSharma <dps158@gmail.com>
2025-07-20 13:57:24 +0530

    project folder  class

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="document_portal",
-    author="Sunny Savita",
+    author="DP Sharma",
     version="0.1",
     packages=find_packages(),
 )
